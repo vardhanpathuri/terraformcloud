e184bbb4aa9a54a2f7c195787d9f55aa1f8622ae
vardhanpathuri <143291387+vardhanpathuri@users.noreply.github.com>
2023-11-29 12:15:51 -0500

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,14 +1,9 @@
 provider "aws" {
   region = "us-east-2"
-  	
 }
 
 resource "aws_instance" "intro" {
   ami                    = "ami-03657b56516ab7912"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-2a"
-  
 }
-
-
-
