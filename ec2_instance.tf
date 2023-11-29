f96451e9a5e7802b1172c81a94d4316becd22d10
vardhanpathuri <143291387+vardhanpathuri@users.noreply.github.com>
2023-11-29 16:51:20 -0500

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "intro" {
-  ami                    = "ami-0230bd60aa48260c6"
+  ami                    = "ami-0fa1ca9559f1892ec"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-2a"
 }
