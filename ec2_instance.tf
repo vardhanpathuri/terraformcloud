9c5161d8f585eef019807216c0b1bdf9dfb2569e
vardhanpathuri <143291387+vardhanpathuri@users.noreply.github.com>
2023-12-01 17:26:21 -0500

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -5,5 +5,5 @@ resource "aws_instance" "intro" {
   ami                    = "ami-0230bd60aa48260c6"
   instance_type          = "t2.micro"
   subnet_id = aws_subnet.demo_subnet.id
-  vpc_security_group_ids = aws_security_group.demo-vpc-sg.id
+  vpc_security_group_ids = [aws_security_group.demo-vpc-sg.id]
 }
