b8dde1b01b19ab619f0f3fdd82665919ef090e32
sai <pathurisaivardhan@gmail.com>
2023-12-01 17:04:13 -0500

    vpc

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -4,4 +4,6 @@ provider "aws" {
 resource "aws_instance" "intro" {
   ami                    = "ami-0230bd60aa48260c6"
   instance_type          = "t2.micro"
+  subnet_id = aws_subnet.demo_subnet.id
+  vpc_security_group_ids = aws_security_group.demo-vpc-sg.id
 }
